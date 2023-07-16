16fdbe7b1ba4cb9fc9eecaa42b03c22d88241ae8
Maciej <MaciejJ1999@interia.pl>
2023-07-16 15:04:18 +0200

    Added News model

--- members/models.py
+++ members/models.py
@@ -10,13 +10,13 @@ from django.urls import reverse
 #Image model
 
 class Image(models.Model):
-    description = models.TextField(null =  True, blank = True)
+    #description = models.TextField(null =  True, blank = True)
+    title = models.TextField(null =  True, blank = False)
     altTxt = models.TextField(null = True, blank = True)
-    title = models.TimeField(null = True, blank = True)
 
     squareImg = ResizedImageField(size = [1000, 1000], crop = ['middle', 'center'], default = 'defaultS.jpg', upload_to = 'square')
-    horiImg = ResizedImageField(size = [1920, 1080], crop = ['middle', 'center'], default = 'defaultH.jpg', upload_to = 'horizontal')
-    vertiImg = ResizedImageField(size = [1080, 1920], crop = ['middle', 'center'], default = 'defaultV.jpg', upload_to = 'vertical')
+    #horiImg = ResizedImageField(size = [1920, 1080], crop = ['middle', 'center'], default = 'defaultH.jpg', upload_to = 'horizontal')
+    #vertiImg = ResizedImageField(size = [1080, 1920], crop = ['middle', 'center'], default = 'defaultV.jpg', upload_to = 'vertical')
 
     uniqueId = models.CharField(null=True, blank=True, max_length=100)
     slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
@@ -30,6 +30,8 @@ class Image(models.Model):
         return reverse("model_detail", kwargs={'slug': self.slug})
     
     def save(self, *args, **kwargs):
+        if self.altTxt is None:
+            self.altTxt = self.title        
         if self.date_created is None:
             self.date_created = timezone.localtime(timezone.now())
         if self.uniqueId is None:
@@ -41,10 +43,11 @@ class Image(models.Model):
         self.last_updated = timezone.localtime(timezone.now())
         super(Image, self).save(*args, **kwargs)
 
+#Puppies
 
 class Puppies(models.Model):
-    name = models.TextField(null =  True, blank = True)
-    description = models.TextField(null =  True, blank = True)
+    name = models.TextField(null =  True, blank = False)
+    description = models.TextField(null =  True, blank = False)
     altTxt = models.TextField(null = True, blank = True)
 
     squareImg = ResizedImageField(size = [1000, 1000], crop = ['middle', 'center'], default = 'defaultS.jpg', upload_to = 'square')
@@ -61,6 +64,8 @@ class Puppies(models.Model):
         return reverse("model_detail", kwargs={'slug': self.slug})
     
     def save(self, *args, **kwargs):
+        if self.altTxt is None:
+            self.altTxt = self.title   
         if self.date_created is None:
             self.date_created = timezone.localtime(timezone.now())
         if self.uniqueId is None:
@@ -71,3 +76,36 @@ class Puppies(models.Model):
         self.slug = slugify('{} {}'.format(self.name, self.uniqueId))
         self.last_updated = timezone.localtime(timezone.now())
         super(Puppies, self).save(*args, **kwargs)
+
+class News(models.Model):
+    title = models.TextField(null =  True, blank = False)
+    contents = models.TextField(null =  True, blank = False)
+    altTxt = models.TextField(null = True, blank = True)
+
+    squareImg = ResizedImageField(size = [1000, 1000], crop = ['middle', 'center'], default = 'defaultS.jpg', upload_to = 'square')
+    #horiImg = ResizedImageField(size = [1920, 1080], crop = ['middle', 'center'], default = 'defaultH.jpg', upload_to = 'horizontal')
+    #vertiImg = ResizedImageField(size = [1080, 1920], crop = ['middle', 'center'], default = 'defaultV.jpg', upload_to = 'vertical')
+
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
+    def __str__(self) -> str:
+        return '{} {}'.format(self.title, self.uniqueId)
+    
+    def get_absolute_url(self):
+        return reverse("model_detail", kwargs={'slug': self.slug})
+    
+    def save(self, *args, **kwargs):
+        if self.altTxt is None:
+            self.altTxt = self.title        
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+
+
+        self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+        self.last_updated = timezone.localtime(timezone.now())
+        super(News, self).save(*args, **kwargs)
